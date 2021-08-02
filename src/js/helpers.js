f62e6c10a51ab57781d2a489661b32e027426e0d
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-02 11:06:29 +0530

    ErrorHandling

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,13 +1,25 @@
+import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+
+
 export const getJSON = async function (url) {
 
     try {
 
-        const res = await fetch(url);
+        // const res = await fetch(url);
 
         // 
         // json method is availiable on all the response objects..
+        const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
         const data = await res.json();
-
         if (!res.ok) {
             throw new Error(`${data.message} (${res.status})`)
         }
@@ -16,5 +28,4 @@ export const getJSON = async function (url) {
     } catch (err) {
         throw err;
     }
-
 }
