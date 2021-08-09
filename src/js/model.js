af42ff35db11c9a618e561b901a51c0e0e8c8823
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-09 13:07:05 +0530

    bookmarked-UI-Bugs-fixed and remove bookmark added

--- src/js/model.js
+++ src/js/model.js
@@ -81,3 +81,9 @@ export const addBookmark = (recipe) => {
     // mark current recipe as bookmarked -->
     if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
 };
+
+export const removeBookmark = (id) => {
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+    if (id === state.recipe.id) state.recipe.bookmarked = false;
+}
